eed5927465756ce384bf4a2b9ed997e1f131625c
Reim-developer <contact.kaxtr@gmail.com>
2025-01-05 19:45:27 +0700

    Fixed error 'discord.ext.commands.errors.MissingRequiredArgument: user is a required argument that is missing.' in cogs/ulti/userinfo.py

--- cogs/ulti/userinfo.py
+++ cogs/ulti/userinfo.py
@@ -8,7 +8,24 @@ class Userinfo(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def userinfo(self, ctx: commands.Context, user: discord.Member) -> None:
+    async def userinfo(self, ctx: commands.Context, user: discord.Member = None) -> None:
+        if user is None:
+            embed = discord.Embed(
+            title = f"Thông tin về {ctx.author.name}",
+            description = (
+                f"* **Tên người dùng:** {ctx.author.name}\n" +\
+                f"* **ID:** `{ctx.author.id}`\n" +\
+                f"* **Ngày tạo tài khoản:** `{ctx.author.created_at.strftime('%d:%m:%Y')}`\n" +\
+                f"* **Avatar:** [Tải xuống]({ctx.author.avatar.url})"
+            ),
+            colour = 0x696969,
+            timestamp = datetime.datetime.now()
+        )
+            embed.set_thumbnail(url = ctx.author.avatar.url)
+
+            await ctx.channel.send(embed = embed)
+            return
+
         embed = discord.Embed(
             title = f"Thông tin về {user.name}",
             description = (
