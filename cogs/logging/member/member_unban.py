04dafa5d0bceb93550268ea745aa361b03a92e19
Reim-developer <contact.kaxtr@gmail.com>
2025-01-06 11:08:21 +0700

    Add member_unban.py to -> logging/member

---
+++ cogs/logging/member/member_unban.py
@@ -0,0 +1,27 @@
+import discord
+import datetime
+from discord.ext import commands
+
+class MemberUnban(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild: discord.Guild, user: discord.User) -> None:
+        LOG_CHANNEL_ID = 1047035958857564170
+
+        embed = discord.Embed(
+            title = "Một người dùng vừa được gỡ lệnh cấm khỏi server",
+            description = (
+                f"* **Username:** {user.name}\n" +\
+                f"* **ID:** `{user.id}`\n" +\
+                f"* **Gỡ lệnh cấm vào:** <t:{int(datetime.datetime.now().timestamp())}>"
+            ),
+            color = 0x212121,
+            timestamp = datetime.datetime.now()
+        )
+        
+        await guild.get_channel(LOG_CHANNEL_ID).send(embed = embed)
+
+async def setup(bot) -> None:
+    await bot.add_cog(MemberUnban(bot))
