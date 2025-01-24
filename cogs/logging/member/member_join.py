c0be475a20834987e9176aa8c4fffc713ec80f14
Reim-developer <contact.kaxtr@gmail.com>
2025-01-24 18:11:58 +0700

    Migration this notification embed to English

--- cogs/logging/member/member_join.py
+++ cogs/logging/member/member_join.py
@@ -1,3 +1,11 @@
+r"""
+        AWESOME DISCORD BOT
+        This project is licensed under GPL-3.0 License
+        https://www.gnu.org/licenses/gpl-3.0.html
+        Contribution:
+            - Reim-developer
+"""
+
 import discord
 import datetime
 from discord.ext import commands
@@ -12,11 +20,11 @@ class MemberJoin(commands.Cog):
         LOG_CHANNEL_ID = 1047035958857564170 # WARNING: Replace with your discord log channel
 
         embed = discord.Embed(
-            title = "Vừa có một thành viên mới tham gia server",
+            title = "A new member has joined the server",
             description = (
                 f"* **Username:** {member.name}\n" +\
                 f"* **ID:** `{member.id}`\n" +\
-                f"* **Tham gia vào lúc: <t:{int(datetime.datetime.now().timestamp())}>**"
+                f"* **Joined at: <t:{int(datetime.datetime.now().timestamp())}>**"
             ),
             color = 0x212121,
             timestamp = datetime.datetime.now()
